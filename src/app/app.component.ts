aebda3152022c47299326d4032b2f775dfc3cfc5
resourcegrouphdl <resourcegrouphdl@gmail.com>
2025-05-01 15:53:11 -0500

    fix menu peincipal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, Router } from '@angular/router';
 import { IMenu } from './interfaces/i-menu';
-import { menu } from './interfaces/i-menu copy';
+import { menu } from './interfaces/i-menu';
 import {MatIconModule} from '@angular/material/icon';
 import { TransitionService } from './shared/transition.service';
 import { NgClass } from '@angular/common';
